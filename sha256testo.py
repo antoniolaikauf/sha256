d57620c5da2f8e9e7e8b8849446ec8a3c708db9c
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-07-23 15:56:47 +0200

    fix str

--- sha256testo.py
+++ sha256testo.py
@@ -65,7 +65,7 @@ def padding(bits, lengthM):
 
     return bits
 
-message=input_bit('ciao')
+# message=input_bit('ciao')
 # print(padding(message, len(message)))
 
 def chunks(bits,chunk=8): #divide in chunk 512 bits a 32 bits
