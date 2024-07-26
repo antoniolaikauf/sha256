330ab54e5e8937638b36f9d9c049b2847c49f08e
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-07-26 18:16:40 +0200

    digest corretto

--- sha256.py
+++ sha256.py
@@ -1,15 +1,15 @@
 '''
 sha256 with module
 '''
-
+words=b'ciao'
 from cryptography.hazmat.primitives import hashes
 
 sha256=hashes.Hash(hashes.SHA256())
-sha256.update(b'ciattttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt')
-print(sha256.finalize().hex(), 'parola come input "tutti"')
+sha256.update(words)
+print(sha256.finalize().hex(), f'parola come input "ciao"')
 
 import hashlib
 
 SHA256= hashlib.sha256()
-SHA256.update(b'ciattttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt')
-print(SHA256.hexdigest(), 'parola come input "tutte"')
+SHA256.update(words)
+print(SHA256.hexdigest(), 'parola come input "ciao"')
